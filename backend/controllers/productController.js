acdbaf09c42e685d584649d2e2a18851dc81dc33
ashutoshsingh1711 <43945682+ashutoshsingh1711@users.noreply.github.com>
2022-01-02 17:31:23 +0530

    api features- pagination and search, filter

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,20 +1,82 @@
+const catchAsyncError = require("../middleware/catchAsyncError");
 const Product = require("../models/productModel");
+const ApiFeatures = require("../utils/apifeatures");
+const Errorhandler = require("../utils/errorhandler");
 
 //Create Product -- Admin
-exports.createProduct = async (req, res) => {
+exports.createProduct = catchAsyncError(async (req, res) => {
   const product = await Product.create(req.body);
 
   res.status(201).json({
     success: true,
     product,
   });
-};
+});
 
 //Get all Products
-exports.getAllProducts = async (req, res) => {
-  const products = await Product.find();
+exports.getAllProducts = catchAsyncError(async (req, res) => {
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments;
+  const apiFeatures = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await apiFeatures.query;
   res.status(200).json({
     success: true,
     products,
   });
-};
+});
+
+//Get product Details
+
+exports.getProductDetails = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new Errorhandler("Product Not Found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+    productCount,
+  });
+});
+
+//Update Product -- admin
+
+exports.updateProduct = catchAsyncError(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new Errorhandler("Product Not Found", 404));
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+//Delete Product
+
+exports.deleteProduct = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new Errorhandler("Product Not Found", 404));
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product deleted successfully",
+  });
+});
---
+++ backend/middleware/error.js
@@ -0,0 +1,18 @@
+const Errorhandler = require("../utils/errorhandler");
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal server error";
+
+  //Wrong MongoDB id error "cast error"
+
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new Errorhandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
